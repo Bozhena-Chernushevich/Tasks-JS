3df449c422b2fc11d6f5644e98ad4c9a02233dea
Bozhena-Chernushevich <chernushevich001@gmail.com>
2022-08-19 12:08:12 +0300

    Added js tasks

---
+++ 3 циклы/script.js
@@ -0,0 +1,52 @@
+// let i = 0;
+
+// while (i <= 100) {
+//   document.write(i + '<br>'); 
+//   i++;
+// }
+// for (i = 0; i<=100; i++) {
+//   document.write(i + '<br>'); 
+// }
+
+
+// let i = 1;
+
+// while (i < 101) {
+// alert ( i );
+//   i++;
+// }
+
+
+// let i = 11;
+
+// while (i<34){
+//   alert ( i );
+//   i++;
+// }
+
+
+// for (let i = 2; i<=100; i++) {
+
+//   if (i % 2 == 0){
+//     alert (i);
+//   }
+// }
+
+
+// let sum = 0;
+
+// for ( let i = 0; i <= 100; i++) {
+//   sum += i;
+// }
+// alert (sum);
+
+
+let n = 1000;
+let num = 0;
+
+while (n>=50) {
+  n/=2;
+  num++;
+}
+console.log(n);
+console.log(num);
---
+++ 4 switch/script.js
@@ -0,0 +1,16 @@
+let num = +prompt ("Введите число от 1 до 4");
+
+switch(num) {
+  case 1:
+    alert( 'Зима' );
+    break;
+  case 2:
+    alert( 'Весна' );
+    break;
+  case 3:
+    alert( 'Лето' );
+    break;
+  case 4:
+    alert( 'Осень' );
+    break;
+}
